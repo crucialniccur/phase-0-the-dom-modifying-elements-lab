459494c9683c5513d55555ef4112e5bb3e714a1c
Mati Joseph <crucialniccur@gmail.com>
2025-03-16 04:48:29 +0300

    add the id of victory to the newHeader

--- index.js
+++ index.js
@@ -8,3 +8,7 @@ main.remove();
 //create newHeader , a <h1> node
 const newHeader = document.createElement("h1");
 document.body.append(newHeader);
+
+// add id victoy to newHeader
+// newHeader.id = "victory";
+newHeader.setAttribute("id", "victory");
